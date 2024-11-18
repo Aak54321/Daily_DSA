515d19bde0407f457f90ec3481df3cad178df7cf
Aakash <saakash2k4@gmail.com>
2024-11-18 14:24:42 +0530

    algo

--- 18_NOV/BubbleSort.java
+++ 18_NOV/BubbleSort.java
@@ -1,3 +1,22 @@
-public class BubbleSort { 
+public class BubbleSort {
+    public static void bubbleSort(int arr[]) {
+        // code here
+        boolean swapped = false;
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    swapped = true;
+                    swap(arr, j, j + 1);
+                }
+            }
+            if (!swapped)
+                break;
+        }
+    }
 
+    public static void swap(int[] arr, int p1, int p2) {
+        int temp = arr[p1];
+        arr[p1] = arr[p2];
+        arr[p2] = temp;
+    }
 }
